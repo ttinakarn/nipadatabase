15a8346663e72507c410a7b7d28ddb0d74efa921
ttinakarn <haiiplara@hotmail.com>
2019-08-27 23:13:23 +0700

    select date เพิ่ม

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const pgp = require('pg-promise')();
 var db = pgp('postgres://grbrxjrhauptjn:19388487ac4fc3ef3eeea4094c17e9aa5fd0774510bdd7db513c481c0ea31d41@ec2-174-129-227-51.compute-1.amazonaws.com:5432/d6b3313j3l8qt5?ssl=true');
 
 function getVitalSigns(req, res) {
-    db.any(`select bednumber, temp, pulse, resp, sbp, dbp, o2sat, eye, verbal, motor, urine, painscore, fallrisk, remark, name, date
+    db.any(`select bednumber, temp, pulse, resp, sbp, dbp, o2sat, eye, verbal, motor, urine, painscore, fallrisk, remark, name
     from treatmenthistory inner join vitalsign
     on vitalsign.an = treatmenthistory.an
     inner join employee 
@@ -27,7 +27,7 @@ function getVitalSigns(req, res) {
 }
 
 function getVitalSignByID(req, res) {
-    db.any(`select bednumber, temp, pulse, resp, sbp, dbp, o2sat, eye, verbal, motor, urine, painscore, fallrisk, remark, name
+    db.any(`select bednumber, temp, pulse, resp, sbp, dbp, o2sat, eye, verbal, motor, urine, painscore, fallrisk, remark, name, date
     from treatmenthistory inner join vitalsign
     on vitalsign.an = treatmenthistory.an
     inner join employee 
