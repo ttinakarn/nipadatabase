20b129cb0f40d2de32294288f4f72959e5d52cfb
ttinakarn <haiiplara@hotmail.com>
2019-09-25 15:21:10 +0700

    edit sql

--- database.js
+++ database.js
@@ -2,12 +2,12 @@ const pgp = require('pg-promise')();
 var db = pgp('postgres://grbrxjrhauptjn:19388487ac4fc3ef3eeea4094c17e9aa5fd0774510bdd7db513c481c0ea31d41@ec2-174-129-227-51.compute-1.amazonaws.com:5432/d6b3313j3l8qt5?ssl=true');
 
 function getVitalSigns(req, res) {
-    db.any(`select vitalsign.an, date, bednumber, temp, pulse, resp, sbp, dbp, o2sat, eye, verbal, motor, urine, painscore, fallrisk, remark, name
+    db.any(`select vitalsign.an, bednumber, temp, pulse, resp, sbp, dbp, o2sat, eye, verbal, motor, urine, painscore, fallrisk, remark, name
     from treatmenthistory inner join vitalsign
     on vitalsign.an = treatmenthistory.an
     inner join employee 
     on vitalsign.empid = employee.empid
-    order by bednumber, date DESC`)
+    order by bednumber`)
         .then(function (data) {
             res.status(200)
                 .json({
@@ -284,92 +284,6 @@ function getLastestVS(req, res) {
                 });
         })
 }
-
-// function updatevitalsign(req, res) {
-//         db.none('update vitalsign set an=${an}, temp=${temp}, pulse=${pulse}, resp=${resp}, pulse=${pulse}, sbp=${sbp}, pulse=${pulse}, dbp=${dbp}, o2sat=${o2sat}, eye=${eye}, verbal=${verbal}, motor=${motor}, urine=${urine}, painscore=${painscore}, fallrisk=${fallrisk}, remark=${remark}, eye=${eye}, date=${date}' +
-//         'where id=' + req.params.id, req.body)
-//             .then(function (data) {
-//                 res.status(200)
-//                     .json({
-//                         status: 'success',
-//                         message: 'Updated vitalsign'
-//                     });
-//             })
-//             .catch(function (error) {
-//                 console.log('ERROR:', error)
-//             })
-//     }
-
-// function getAllProducts(req, res) {
-//     db.any('select * from products')
-//         .then(function (data) {
-//             res.status(200)
-//                 .json({
-//                     status: 'success',
-//                     data: data,
-//                     message: 'Retrieved ALL products'
-//                 });
-//         })
-//         .catch(function (error) {
-//             console.log(error);
-//             res.status(500)
-//                 .json({
-//                     status: 'failed',
-//                     message: 'Failed to retrieved ALL products'
-//                 });
-//         })
-// }
-
-// function getProductByID(req, res) {
-//     db.any('select * from products where id =' + req.params.id)
-//         .then(function (data) {
-//             res.status(200)
-//                 .json({
-//                     status: 'success',
-//                     data: data,
-//                     message: 'Retrieved products id:' + req.params.id
-//                 });
-//         })
-//         .catch(function (error) {
-//             res.status(500)
-//                 .json({
-//                     status: 'failed',
-//                     message: 'Failed to retrieved products id:' +
-//                         req.params.id
-//                 });
-//         })
-// }
-
-// function insertProduct(req, res) {
-//     db.none('insert into products(id, title, price, created_at, tags)' +
-//         'values(${id}, ${title}, ${price}, ${created_at}, ${tags})',
-//         req.body)
-//         .then(function (data) {
-//             res.status(200)
-//                 .json({
-//                     status: 'success',
-//                     message: 'Inserted one product'
-//                 });
-//         })
-//         .catch(function (error) {
-//             console.log('ERROR:', error)
-//         })
-// }
-
-// function updateProduct(req, res) {
-//     db.none('update products set title=${title}, price=${price}, tags=${tags} ' +
-//     'where id=' + req.params.id, req.body)
-//         .then(function (data) {
-//             res.status(200)
-//                 .json({
-//                     status: 'success',
-//                     message: 'Updated product'
-//                 });
-//         })
-//         .catch(function (error) {
-//             console.log('ERROR:', error)
-//         })
-// }
 
 module.exports = {
     getVitalSigns,
