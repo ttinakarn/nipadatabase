81fe489896e8850d8b2ec21900ffff98052a8682
wanatip <s5930213049@phuket.psu.ac.th>
2019-10-05 16:03:24 +0700

    เปลี่ยน temp เป็น True

--- database.js
+++ database.js
@@ -101,7 +101,7 @@ function insertVitalSigns(req, res, next) {
             var vs = ['temp', 'pulse', 'resp', 'sbp', 'dbp', 'o2sat', 'eye', 'verbal', 'motor', 'urine', 'painscore', 'fallrisk', 'remark' ]
             const updatedData = {
                 bednumber: 1, status: [
-                    { temp: false },
+                    { temp: true },
                     { pulse: false },
                     { resp: false},
                     { sbp: true },
