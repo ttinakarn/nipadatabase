e2e8dbcabd2859171a074f7fe2eec5ebd149fc31
ttinakarn <haiiplara@hotmail.com>
2019-11-28 14:34:23 +0700

    get bed number and get bed info

--- database.js
+++ database.js
@@ -155,11 +155,9 @@ function insertVitalSigns(req, res, next) {
 }
 
 function getBedNumber(req, res) {
-    db.any(`select vitalsign.an, bednumber, max(date)
-    from vitalsign inner join treatmenthistory
-    on vitalsign.an = treatmenthistory.an
-    where treatmenthistory.dischargedate is null
-    group by bednumber, vitalsign.an
+    db.any(`select treatmenthistory.an, bednumber
+    from treatmenthistory
+	where treatmenthistory.dischargedate is null
     order by bednumber`)
         .then(function (data) {
             res.status(200)
@@ -180,13 +178,11 @@ function getBedNumber(req, res) {
 }
 
 function getBedInfo(req, res) {
-    db.any(`select patient.hn, treatmenthistory.an, title, name, surname, dob, admitdate, last_value(remark) over (order by remark) as max
+    db.any(`select patient.hn, treatmenthistory.an, title, name, surname, dob, admitdate
     from patient inner join treatmenthistory
     on patient.hn = treatmenthistory.hn
-    inner join vitalsign
-    on treatmenthistory.an = vitalsign.an
-    where treatmenthistory.an = '` + req.params.id + `'
-    group by patient.hn, treatmenthistory.an, remark
+    where treatmenthistory.an = '` + req.params.id +`'
+    group by patient.hn, treatmenthistory.an
 	limit 1`)
         .then(function (data) {
             res.status(200)
